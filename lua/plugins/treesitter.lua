80a534bf783ae5568817111bc4aa5c8049c9ee6c
Damilola O. <82976159+ddddami@users.noreply.github.com>
2024-12-18 16:16:08 +0100

    Add some parsers via treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,7 +7,20 @@ return {
     ensure_installed = {
       "lua",
       "vim",
-      "astro"
+      "astro",
+      "go",
+      "php",
+      "javascript",
+      "typescript",
+      "python",
+      "bash",
+      "vue",
+      "markdown",
+      "json",
+      "json5",
+      "yaml",
+      "html",
+      "css",
       -- add more arguments for adding more treesitter parsers
     },
   },
